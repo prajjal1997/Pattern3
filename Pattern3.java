89fb693a2d0f107e3e77bffa9ee3708554536f7e
prajjal saxena <52135244+prajjal1997@users.noreply.github.com>
2019-06-27 00:05:03 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,21 @@
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=5;i++)                                           
+		{                                                  //    *****
+			for(j=1;j<=5;j++) {                             //    ****
+			if(j>=i)                                       //      ***
+				System.out.print("*");                     //       **
+			else                                          //         *
+			System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
